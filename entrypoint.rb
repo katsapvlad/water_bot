967dc38d8ce80cd0dfbe112bdcd56cb2cd6ab61a
katsapvlad <katsapvlad@yandex.by>
2022-11-10 16:00:43 +0300

    Add more debug messages

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,29 +1,45 @@
 # frozen_string_literal: true
 
-require 'dotenv/load'
-require 'erb'
-require 'i18n'
-require 'telegram/bot'
-require 'active_record'
-require_relative 'app/services/responser'
-require_relative 'app/services/router'
-require_relative 'app/modules/inline_button'
-require_relative 'config/constants'
-
-
 logger = Logger.new($stdout)
 
 logger.info('Debug message: 1')
 
-db_config = YAML.safe_load(ERB.new(File.read('config/database.yml.erb')).result)
+require 'dotenv/load'
+
 logger.info('Debug message: 2')
-ActiveRecord::Base.establish_connection(db_config)
+
+require 'erb'
+
 logger.info('Debug message: 3')
-I18n.load_path = Dir["#{File.expand_path('config/locales')}/*.yml"]
+require 'i18n'
+
 logger.info('Debug message: 4')
-I18n.default_locale = :en
+require 'telegram/bot'
 
 logger.info('Debug message: 5')
+require 'active_record'
+
+logger.info('Debug message: 6')
+require_relative 'app/services/responser'
+
+logger.info('Debug message: 7')
+require_relative 'app/services/router'
+
+logger.info('Debug message: 8')
+require_relative 'app/modules/inline_button'
+
+logger.info('Debug message: 9')
+require_relative 'config/constants'
+
+db_config = YAML.safe_load(ERB.new(File.read('config/database.yml.erb')).result)
+logger.info('Debug message: 10')
+ActiveRecord::Base.establish_connection(db_config)
+logger.info('Debug message: 11')
+I18n.load_path = Dir["#{File.expand_path('config/locales')}/*.yml"]
+logger.info('Debug message: 12')
+I18n.default_locale = :en
+
+logger.info('Debug message: 13')
 
 logger.info('Bot started')
 
